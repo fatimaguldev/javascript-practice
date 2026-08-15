7aa86cf4662fa8a51369da86faa2ee5d1eb7de6b
fatima gul <fatimaguldev@gmail.com>
2026-08-15 21:14:01 +0500

    learned JS variable changes

---
+++ 01_Basics/changes.js
@@ -0,0 +1,13 @@
+let favMovie = "SpiderMan"
+
+favMovie = "BatMan"
+
+console.log(favMovie); 
+
+// let keyword can be change
+
+const userName = "fatimagul2110"
+
+userName = "fatimagul2111"
+
+console.log(userName);
