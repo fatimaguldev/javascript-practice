7c3a30b73b4321ec45e6f8fb755cecdf1b128f56
fatima gul <fatimaguldev@gmail.com>
2026-08-15 21:13:39 +0500

    learned JS printing and console.log

---
+++ 01_Basics/printing.js
@@ -0,0 +1,8 @@
+console.log("Hello Fatima");
+console.log("Hello Fatima");
+process.stdout.write("hello world")
+process.stdout.write("hello world"); 
+// the process.stdout.write have no build in line
+
+
+console.table({city: "peshawar"})
