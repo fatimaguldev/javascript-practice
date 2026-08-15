20cb19866e594d5b5ec79aa4136ff3b924a2267d
fatima gul <fatimaguldev@gmail.com>
2026-08-15 23:13:27 +0500

    added JS app practice file

---
+++ 01_Basics/app.js
@@ -0,0 +1,7 @@
+// i have connected this to the html files
+
+// console.log("Hello Fatima Gul");
+// console.log("This is my JS journey");
+let a = 10;
+let b = 20;
+console.log("sum is : ", a + b);
