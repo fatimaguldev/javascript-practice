eea587948db6639dfeff7e63c17e2f9aa394d5d6
fatima gul <fatimaguldev@gmail.com>
2026-08-16 23:57:00 +0500

    learned JS if statement

---
+++ 01_Basics/ifStatement.js
@@ -0,0 +1,19 @@
+console.log("Before my if statement:");
+let age = 12;
+// age = 17;
+if (age >= 18) {
+    console.log("You can Vote!");
+    console.log("You can drive");
+}
+if (age < 18) { 
+    console.log("You cannot Vote");
+    
+}
+console.log("After my if statement:");
+    
+let firstName = "Salam"
+if (firstName == "Fatima Gul")
+{
+ console.log(`Welcome ${firstName}`);
+ 
+}
