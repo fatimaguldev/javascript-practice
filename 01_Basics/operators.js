ac0957bd8283044e1cab3b00bf8ae3f9e1813caa
fatima gul <fatimaguldev@gmail.com>
2026-08-16 23:57:15 +0500

    learned JS operators

---
+++ 01_Basics/operators.js
@@ -0,0 +1,71 @@
+// Arithmetic Operator
+let a = 10;
+let b = 20;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+
+// Unary Operators 
+let x = 5;
+let y = 5;
+console.log(++x);
+
+// Comparison Operators
+
+let age = 23;
+console.log(age > 18);
+
+age = 18;
+console.log(age > 18);
+console.log(age);
+
+console.log(age >= 20);
+console.log(age <= 20);
+
+console.log(5 == 5);
+console.log(5 != 5);
+
+let n = 5
+let str = '5'
+console.log(typeof n);
+console.log(typeof str);
+console.log(n == str);
+console.log(4 != str);
+console.log(n == str);
+
+// == != it does not compare type of operator value for strict and to find type as well we will use ===
+
+let s = 6;
+let str = '6'
+console.log(typeof s);
+console.log(typeof t);
+console.log(s == str);
+console.log(s === str);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
