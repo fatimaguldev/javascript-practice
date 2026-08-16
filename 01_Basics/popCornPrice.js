47a252aaa6927d6d15c3a75761098ff069916a02
fatima gul <fatimaguldev@gmail.com>
2026-08-16 23:57:30 +0500

    built popcorn price calculator

---
+++ 01_Basics/popCornPrice.js
@@ -0,0 +1,12 @@
+// to create a system to calculate popcorn prices based on the size of customer asked for
+
+let size = "S";
+if ((size == "XL")) {
+  console.log("Price is RS. 250");
+} else if ((size == "L")) {
+  console.log("Price is RS. 200");
+} else if ((size == "M")) {
+  console.log("Price is RS. 100");
+} else {
+  console.log("Price is RS. 50");
+}
