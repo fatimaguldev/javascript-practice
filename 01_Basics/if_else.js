5aea4b756511716b64a1cacaeaa0859554df5457
fatima gul <fatimaguldev@gmail.com>
2026-08-16 23:57:05 +0500

    learned JS if else statement

---
+++ 01_Basics/if_else.js
@@ -0,0 +1,17 @@
+let age = 14;
+if (age >= 18) {
+    console.log("you can vote");
+}
+else if (age < 18) {
+    console.log("you cannot vote!");
+}
+
+let marks = 40;
+if (marks >= 70) {
+    console.log("you are pass!");
+}
+else if (marks <= 50) {
+    console.log("Better Luck Next Time!"); 
+} else if (marks <= 69) {
+    console.log("you are fail!");
+}
