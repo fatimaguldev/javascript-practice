fd40657faccbe00560c85d2a23dc534c25414e67
fatima gul <fatimaguldev@gmail.com>
2026-08-16 23:57:37 +0500

    built traffic light logic

---
+++ 01_Basics/TrafficLight.js
@@ -0,0 +1,15 @@
+// create a traffic light system that shows what to do based on color.
+let color = "yellow";
+if (color == "red")
+{
+    console.log("STOP! Light color is red.");  
+}
+
+if (color == "yellow") {
+  console.log("SLOW! Light color is yellow.");
+}
+
+if (color == "green") {
+  console.log("GO! Light color is green.");
+}
+
