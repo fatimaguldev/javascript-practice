b506111b9558139cebbe61d39d6707202c20337b
fatima gul <fatimaguldev@gmail.com>
2026-08-16 23:57:21 +0500

    learned JS non numbers

---
+++ 01_Basics/nonNumbers.js
@@ -0,0 +1,7 @@
+// each number or character on keyboard have a special unicodes so if we compare small letters with capital letters than obivously capital letter will be large like for example the UNICODE for P is 50 and p is 30 so P is greater
+
+// small letters have bigger value as compared to Capital letters
+
+// A=41 B=42....
+
+// a=61 b=62
