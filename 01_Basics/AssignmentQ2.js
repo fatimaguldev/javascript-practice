7cbdd307be7f5d22027778870b28fc2dfc1fd4d9
fatima gul <fatimaguldev@gmail.com>
2026-08-17 23:02:34 +0500

    completed JS assignment Q2

---
+++ 01_Basics/AssignmentQ2.js
@@ -0,0 +1,5 @@
+// take the users name and age is input using prompts then return back the following statement to the user as an alert by substituting their name and age name is a gears old use template literals to print this sentence
+
+let userName = prompt("Enter your username");
+let age = prompt("Enter your age");
+console.log(`${userName}, is ${age} years old`);
