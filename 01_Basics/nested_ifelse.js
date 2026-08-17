a1243e1eeab5c9f9270e03f9ba371af4ccf5324a
fatima gul <fatimaguldev@gmail.com>
2026-08-17 23:01:34 +0500

    learned JS nested if else

---
+++ 01_Basics/nested_ifelse.js
@@ -0,0 +1,13 @@
+let marks =10;
+if (marks >= 33) {
+    console.log("Pass");
+    if (marks >= 80) {
+        console.log("Grade: A1");
+    } else {
+        console.log("Grade: A");
+        
+    }
+    
+} else {
+    console.log("Better luck next time!"); 
+}
