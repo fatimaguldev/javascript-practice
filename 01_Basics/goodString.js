d8b4cfc887ebe5af615219a469760d2c3a4089fc
fatima gul <fatimaguldev@gmail.com>
2026-08-17 23:02:11 +0500

    practiced JS string operations

---
+++ 01_Basics/goodString.js
@@ -0,0 +1,8 @@
+// A "Good String" is the one which start from 'a'and has a length > 3. Write a program to check whter the string is good or not!
+
+let str = "amn";
+if ((str[0] === "a") && (str.length > 3)) {
+  console.log("Good String");
+} else {
+     console.log("Bad String");
+}
