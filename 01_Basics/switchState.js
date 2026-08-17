a8e87b42a007868f710c03c3d5cc44cb0950d0cd
fatima gul <fatimaguldev@gmail.com>
2026-08-17 23:01:39 +0500

    learned JS switch statement

---
+++ 01_Basics/switchState.js
@@ -0,0 +1,19 @@
+
+let color = "pink";
+switch (color) {
+  case "red":
+    console.log("Stop!");
+    break;
+
+  case "yellow":
+    console.log("Slow!");
+    break;
+
+  case "green":
+    console.log("Go!");
+        break;
+    
+  default:
+    console.log("Broken Light");
+    break;
+} 
