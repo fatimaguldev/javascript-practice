3c7a9c31d3b1f4007bc98f3dc3349d426e54f0a7
fatima gul <fatimaguldev@gmail.com>
2026-08-17 23:02:21 +0500

    completed JS assignment Q1

---
+++ 01_Basics/AssignmentQ1.js
@@ -0,0 +1,8 @@
+// create a number variable name with some value no print good if the number is divisible by 10 and print bed if it is not
+
+let num = 25;
+if (num % 10 === 0) {
+    console.log("Good");
+} else {
+    console.log("Bad");
+}
