9121eabdfe41d67f0695a693f93c4ad073ab6f8b
fatima gul <fatimaguldev@gmail.com>
2026-08-17 23:04:03 +0500

    learned JS truthy and falsy values

---
+++ 01_Basics/truthy&falsy.js
@@ -0,0 +1,15 @@
+if (true)
+{
+    console.log("it has true value");
+    
+} else {
+    console.log("it has false value");
+    
+}
+
+// each number and character in JS has a special value may be true or false....so we dont have the need to give them name of true or false....for example in above example if we place 0 the value or output will be false ..becuase zero has false value
+
+// Falsy (Absence / Emptiness): If your wallet has $0, it is effectively empty. If a box is completely empty (like an empty string ""), there is nothing there. JavaScript treats this emptiness or nothingness as a false signal.
+
+
+// Truthy (Presence / Substance): If your wallet has even $1 or -$5 (debt), it is not empty—money is present. If a box has a single crumpled piece of paper inside it, it is not empty. JavaScript treats the presence of anything as a true signal.
