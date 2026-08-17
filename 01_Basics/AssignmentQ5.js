8a4e9bda1183b4d709ff74db7d6344a5c369f1a2
fatima gul <fatimaguldev@gmail.com>
2026-08-17 23:02:51 +0500

    completed JS assignment Q5

---
+++ 01_Basics/AssignmentQ5.js
@@ -0,0 +1,17 @@
+// write a program to find the largest three number
+
+let a = 20;
+let b = 15;
+let c = 100;
+if (a >= b && a >= c) {
+    console.log("a is the largest");
+    
+   
+} else if (b >= a && b >= c) {
+    console.log("b is the largest");
+    
+    
+} else {
+    console.log("c is the largest");
+}
+    
