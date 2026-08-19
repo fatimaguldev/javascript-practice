8cacb49199c24fa072d6e6e059686209114cca86
fatima gul <fatimaguldev@gmail.com>
2026-08-19 23:08:01 +0500

    completed JS practice Q4

---
+++ 01_Basics/practiceQ4.js
@@ -0,0 +1,9 @@
+// convert using splice
+
+let months = ['january', 'july', 'march', 'august'];
+months.splice(0, 2, "july", "june");
+
+
+// output:  ['july', 'june', 'july', 'march', 'august']
+
+
