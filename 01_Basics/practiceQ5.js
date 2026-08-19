87f4d78bbf01a32ff8354f3bc6555010c3e126eb
fatima gul <fatimaguldev@gmail.com>
2026-08-19 23:08:06 +0500

    completed JS practice Q5

---
+++ 01_Basics/practiceQ5.js
@@ -0,0 +1,7 @@
+// return the index of js from the given array, if it was reversed.
+
+let lang = ['C++', 'C', 'html', 'Javascript', 'python', 'java', 'C#', 'sql'];
+
+lang.reverse();
+
+lang.indexOf("Javascript");
