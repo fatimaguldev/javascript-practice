410cdd7fee49443f2379a87111d0d698c3cd1cf8
fatima gul <fatimaguldev@gmail.com>
2026-08-19 23:07:38 +0500

    learned JS sort method

---
+++ 01_Basics/sortArr.js
@@ -0,0 +1,11 @@
+// sort to arrange in ascending and decending
+
+let clr = ["orange", "blue", "green", "red", "pink", "yellow"];
+
+clr.sort(); //it will arrange alphabetically
+ 
+
+let marks = [99, 89, 29, 56, 77, 40];
+marks.sort();
+
+ 
