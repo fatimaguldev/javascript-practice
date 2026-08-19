0addc47463c71f3f87387c0496703cc71b262ffb
fatima gul <fatimaguldev@gmail.com>
2026-08-19 23:06:02 +0500

    learned JS arrays basics

---
+++ 01_Basics/arrays.js
@@ -0,0 +1,23 @@
+// at start we will create the variables like this
+
+// let student1 = "fatima";
+// let student2 = "abdul";
+// let student3 = "ayan";
+
+let students = ["fatima", "abdul", "ayan"];
+
+let nums = [2, 4, 6, 8];
+nums[3];
+nums.length;
+
+
+// example of different types of arrays
+
+let marks = [99, 80, 89, 92, 90];
+let names = ["fatima", "abdul", "ayan"];
+let info = ["fatima", 92, 2.9];  //mixed arrays
+
+// empty arrays
+
+let newArray = [];
+
