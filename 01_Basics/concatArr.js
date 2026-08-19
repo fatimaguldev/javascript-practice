668a44d859f85c1ee08ea2ed41ba6b6f38beae21
fatima gul <fatimaguldev@gmail.com>
2026-08-19 23:07:09 +0500

    learned JS concat method

---
+++ 01_Basics/concatArr.js
@@ -0,0 +1,11 @@
+let primary = ['orange', 'blue', 'green', 'red', 'pink'];
+let secondary = ['yellow', 'voilet', 'black'];
+primary.concat(secondary); //both will merge
+
+// output:  ["orange", "blue", "green", "red", "pink", "yellow", "voilet", "black"];
+
+primary.reverse(); //it will reverse ['orange', 'blue', 'green', 'red', 'pink'];
+
+// output:
+// ["pink", "red", "green", "blue", "orange"];
+
