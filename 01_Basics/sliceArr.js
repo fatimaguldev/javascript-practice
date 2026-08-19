58234f91a54e6b77aab225621754239156f42b75
fatima gul <fatimaguldev@gmail.com>
2026-08-19 23:07:15 +0500

    learned JS slice in arrays

---
+++ 01_Basics/sliceArr.js
@@ -0,0 +1,14 @@
+// gives copies of an array
+
+let clr = ["orange", "blue", "green", "red", "pink", "yellow"];
+
+clr.slice(); //['orange', 'blue', 'green', 'red', 'pink'] it will give all the colors back
+
+clr.slice(2); //it will give result from green to onward
+
+clr.slice(2, 3); //the result will only be green bcz 3rd on is red and we exclude the last element
+
+clr.slice(-2); //that we want only last values 
+clr.slice(l); // l is not valid so it will give [] array
+
+ 
