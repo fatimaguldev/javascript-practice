9924a702f7759aa73e3e75b5ae30ad1efa3b11d4
fatima gul <fatimaguldev@gmail.com>
2026-08-19 23:07:05 +0500

    learned JS includes method

---
+++ 01_Basics/includesArray.js
@@ -0,0 +1,4 @@
+let cars = ["BMW", "Alto", "G-wagon", "Porche"];
+cars.includes("BMW");  //vlue will be ture becz it consiste
+
+cars.includes("Toyota"); //value will be false bcz it does not contain toyota 
