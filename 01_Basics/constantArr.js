0969f7a0860add75d5164b55fd8be8de2eb2de25
fatima gul <fatimaguldev@gmail.com>
2026-08-19 23:06:22 +0500

    learned JS constant arrays

---
+++ 01_Basics/constantArr.js
@@ -0,0 +1,10 @@
+const g = 9.8;
+g = 10; //it will give error bcz g is constant and its value will never be change
+
+// lets give const to arrays
+
+const array = [1, 2, 3, 4, 5, 6];
+// we can do the changes to the arrays values like push pop ....but what we cant completely change the array to a new array  like below
+
+arr = [1, 2, 3, 4, 5, 6];
+// arr will not be change and its address is const and changes can happen to value 123445
