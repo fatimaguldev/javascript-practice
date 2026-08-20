53353ab5eeec587bc9e8da3c23c0834bc5230740
fatima gul <fatimaguldev@gmail.com>
2026-08-21 00:00:11 +0500

    add loops basics

---
+++ 01_Basics/Loops.js
@@ -0,0 +1,18 @@
+// for (let i = 1; i <= 5; i++)
+// {
+//     console.log(i);
+    
+// } //forwar run
+
+// 1
+// VM51:3 2
+// VM51:3 3
+// VM51:3 4
+// VM51:3 5
+// // undefined : output
+
+for (let j = 10; j>=1; j--)
+{
+    console.log(j);
+    
+} //backward
