2ff430d9088310b47585ab8bb918d57754278a0f
fatima gul <fatimaguldev@gmail.com>
2026-08-21 23:52:15 +0500

    learned JS while loop

---
+++ 01_Basics/whileLoop.js
@@ -0,0 +1,19 @@
+let i = 1;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+console.log("backward");
+let k = 5;
+while (k >= 1) {
+  console.log(k);
+  i--;
+}
+
+let j = 0;
+while (j <= 20) {
+    console.log(j);
+    j++
+    
+}
