aaf7ca1ef440963e941748b3cf589b79cd51e260
fatima gul <fatimaguldev@gmail.com>
2026-08-21 23:52:05 +0500

    built print even numbers using loop

--- 01_Basics/printEvenNo.js
+++ 01_Basics/printEvenNo.js
@@ -1,3 +1,10 @@
-for (let i = 2; i <= 10; i = i + 2) {
+for (let i=2; i<=10; i= i+2) {
     console.log(i);
+}
+
+console.log("backward");
+
+for (let j = 10; j>= 2; j = j - 2) {
+    console.log(j);
+    
 }
