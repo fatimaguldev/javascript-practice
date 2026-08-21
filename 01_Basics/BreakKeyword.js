ad46083061d02e99e0ee27782ae2f93ddf0544df
fatima gul <fatimaguldev@gmail.com>
2026-08-21 23:52:27 +0500

    learned JS break keyword

---
+++ 01_Basics/BreakKeyword.js
@@ -0,0 +1,11 @@
+let i = 1;
+while (i <= 5) {
+    if (i == 3) {
+        break;
+    }
+    console.log(i);
+    i++;
+}
+
+console.log("we use break keyword at 3 to not print the other numbers!");
+
