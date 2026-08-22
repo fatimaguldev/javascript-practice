477ae9df0df058deda401fa5cbc1daf3d6a7acc7
fatima gul <fatimaguldev@gmail.com>
2026-08-22 21:20:05 +0500

    learned JS for of loop

---
+++ 01_Basics/for_of_loops.js
@@ -0,0 +1,18 @@
+let fruits = [
+  "apple",
+  "mango",
+  "guava",
+  "pineapple",
+  "peach",
+  "strawberry",
+  "grapes",
+];
+for (fruit of fruits) {
+    console.log(fruit);
+    
+}
+
+for (char of "Fatima Gul") {
+    console.log(char);
+    
+}
