bdcdae4cf0a6bb4d96949c71417c7e6c36db416b
fatima gul <fatimaguldev@gmail.com>
2026-08-22 21:20:11 +0500

    learned JS nested for of loop

---
+++ 01_Basics/Nested_for_of_loop.js
@@ -0,0 +1,12 @@
+let heroes = [
+  ["ironman", "spiderman", "thor"],
+  ["superman", "wonder women", "flash"],
+];
+for (list of heroes) {
+    // console.log(list);
+    for (name of list) {
+        console.log(name);
+        
+    }
+    
+}
