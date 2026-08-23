4373ae78bce47716013a71a1d1d3f5be269431f9
fatima gul <fatimaguldev@gmail.com>
2026-08-23 07:52:41 +0500

    remove duplicate practice files

--- countdown.js
+++
@@ -1,9 +0,0 @@
-// write a "while" loop that counts down from 5 to 1and stores the numbers in an array named 'countdown'
-
-let countdown = [];
-let j = 5;
-while (j>0) {
-    countdown.push(j);
-    j--;
-}
-console.log(countdown);
--- while.js
+++
@@ -1,9 +0,0 @@
-// write a 'while' loop that calcultes the sum of all numbers from 1 to 15 and stored the result in a variable name "sum"
-// GOAL: 1+2+3+4+5
-let sum = 0;
-let i = 1;
-while (i <= 5) {
-    sum = sum + i; //sum +=1;
-    i++;
-}
-console.log(sum);
