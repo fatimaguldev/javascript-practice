119ec6a584df8aeac8a624d1dfe204191f511c0b
fatima gul <fatimaguldev@gmail.com>
2026-08-24 23:55:44 +0500

    solved find digit problem

---
+++ FindDigit.js
@@ -0,0 +1,10 @@
+// s2.WriteaJSprogramtofindthenoofdigitsinanumber.Example:ifnumber=287152,count=6
+
+
+
+let number = 287152;
+
+// Convert the number to a string and check its length
+let count = number.toString().length;
+
+console.log(count); // Output: 6
