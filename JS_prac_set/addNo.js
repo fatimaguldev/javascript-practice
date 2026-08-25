41c4289514a334f2dec8808234177f8cf42e4353
fatima gul <fatimaguldev@gmail.com>
2026-08-25 11:00:03 +0500

    added addNo to JS_prac_set

---
+++ JS_prac_set/addNo.js
@@ -0,0 +1,8 @@
+// write a "do while" loop that adds numbers from 1 to 3 and stores the result in a variable named "total"
+
+let total = 0;
+let k = 1;
+do {
+    total+=k;
+    k++;
+} while (k <=3);
