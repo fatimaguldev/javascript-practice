287572257e7db6bc0f594208c6b1ca80413b3e7e
fatima gul <fatimaguldev@gmail.com>
2026-08-26 23:46:23 +0500

    learned converting and getting object values

---
+++ 01_Basics/converInGetValues.js
@@ -0,0 +1,9 @@
+const obj = {
+    1: "a",
+    2: "b",
+    null: "c",
+    true: "d",
+    undefined: "e",
+}; 
+
+// the above null, true, undefined all are keywords but JS is not studying it here as a keyword.these are taken as as a values bcz they are converted to strings automatically 
