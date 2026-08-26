39c2fff1e73489b052989b667c937f1119993994
fatima gul <fatimaguldev@gmail.com>
2026-08-26 23:49:25 +0500

    built twitter post object practice

---
+++ JS_prac_set/twitterPost.js
@@ -0,0 +1,9 @@
+// create an object literal for the properties of thread /twitter post which includes.
+
+const post = {
+    username: "@Fatimagul",
+    content: "this is my first post in js",
+    likes: 150,
+    reports: 10,
+    tags: ["mediazas", "abdul salam", "fatimagul"]
+};
