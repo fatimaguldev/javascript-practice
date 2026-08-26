115bc06d65ef91b1615b5df1bc7d790f4b8e3be3
fatima gul <fatimaguldev@gmail.com>
2026-08-26 23:46:46 +0500

    learned getting values in object literals

---
+++ 01_Basics/getValuesinObjectsLiterals.js
@@ -0,0 +1,10 @@
+const student = {
+    name: "fatima gul", 
+    marks: 98.0,
+}
+
+// if we want to get perticular value of a student we can do that in the following way:
+
+// student["name"] here the 'name' is a key
+// student.name;
+
