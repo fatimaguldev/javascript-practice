87098c33443d60cd0a63537f3ffa75765f8b1164
fatima gul <fatimaguldev@gmail.com>
2026-08-27 23:39:49 +0500

    completed function practice 1

---
+++ 01_Basics/FuncPrac1.js
@@ -0,0 +1,10 @@
+// Create a poem that prints poem
+
+function poem() {
+    console.log("Twinkle twinkle little star");
+    console.log("How i wonder what you are?");
+    console.log("Up above the world so high");
+    console.log("Like a diamond in the sky");
+}
+
+poem();
