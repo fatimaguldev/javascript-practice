c0c63bbacb23e4d7c452712588351f8b432dc402
fatima gul <fatimaguldev@gmail.com>
2026-08-27 23:41:10 +0500

    updated object practice 2

---
+++ JS_prac_set/ObjectPrac2.js
@@ -0,0 +1,7 @@
+// Generate a random number between 1 to 5
+let random = Math.floor(Math.random() * 5) + 1;
+
+// result will be any number from 1 to 5
+
+// if we want to generate numbers only from 20-25 then 
+let num = Math.floor(Math.random() * 5) + 20;
