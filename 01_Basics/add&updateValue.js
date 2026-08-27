4daafc2819211aa7350ba2e9046726e609d9584d
fatima gul <fatimaguldev@gmail.com>
2026-08-27 23:40:04 +0500

    updated add and update value

---
+++ 01_Basics/add&updateValue.js
@@ -0,0 +1,12 @@
+const student = {
+    name: "fatima Gul", 
+    age: 21,
+    city: "Peshawar",
+    uni: "SSBMW",
+    marks: 92.9,
+};
+
+// Change the city to Islamabad  /////////    code will be: student.city = "Islamabad";
+// add a new property "female" ///////////     student.gender = "female";
+// Change the marks to A+      //////////      student.marks = "A+";
+// if we want to delete       ///////////      delete student.marks;
