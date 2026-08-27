aaaec50b4305d3f9e663a6d6f39a1d5f6b945421
fatima gul <fatimaguldev@gmail.com>
2026-08-27 23:40:14 +0500

    updated nested object

---
+++ 01_Basics/nestedObject.js
@@ -0,0 +1,14 @@
+const studentInfo = {
+    fatima: {
+        marks: 94.5,
+        city: "Peshawar",
+    },  
+    Salam: {
+        marks: 90.9,
+        city: "Islamabad",
+    },
+    Shazadi: {
+        marks: 95,
+        city: "Karachi",
+    },
+};
