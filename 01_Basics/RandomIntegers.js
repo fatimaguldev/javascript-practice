a11887596764770b96e8365634e09038cda29da8
fatima gul <fatimaguldev@gmail.com>
2026-08-27 23:39:59 +0500

    updated random integers

---
+++ 01_Basics/RandomIntegers.js
@@ -0,0 +1,14 @@
+// From 1 to 10
+let step1 = Math.random();  //0.23176204473216722 
+// step2:
+num = num * 10; //0.23176204473216722*10
+// step3:
+num = num.floor(num);  
+num = num + 1;
+// Math.floor(Math.random() *10);
+
+
+// from 1 to 10
+
+let random = Math.floor(Math.random() * 10) + 1;
+// result : Random = 4
