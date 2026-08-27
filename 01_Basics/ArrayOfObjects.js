79fa853d1b4413f93ec730502363c7695bd3efc4
fatima gul <fatimaguldev@gmail.com>
2026-08-27 23:39:54 +0500

    updated array of objects

---
+++ 01_Basics/ArrayOfObjects.js
@@ -0,0 +1,19 @@
+// storing information of multiple students
+
+const studentInfo = [
+  {
+    name: "Fatima",
+    marks: 94.5,
+    city: "Peshawar",
+  },
+    {
+    name: "Salam",
+    marks: 90.9,
+    city: "Islamabad",
+  },
+    {
+    name: "Shazadi",
+    marks: 95,
+    city: "Karachi",
+  },
+];
