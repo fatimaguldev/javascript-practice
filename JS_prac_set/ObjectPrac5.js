f71bb6a7dbf4032ddc9ca13f5a5296238ec3ac44
fatima gul <fatimaguldev@gmail.com>
2026-08-27 23:41:27 +0500

    updated object practice 5

---
+++ JS_prac_set/ObjectPrac5.js
@@ -0,0 +1,10 @@
+// Create an object Person with their name,age and city. Edit their city’so riginal value to change it to “New York”.Add a new property country and set it to the United State
+
+let person = {
+    name: "fatima gul",
+    age: 21,
+    city:"peshawar",
+};
+
+person.city = "New York";
+person.country = "United State";
