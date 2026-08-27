436cf76a065aa9045269be6ca17b158b8de1ce02
fatima gul <fatimaguldev@gmail.com>
2026-08-27 23:40:09 +0500

    updated math object

---
+++ 01_Basics/mathObject.js
@@ -0,0 +1,8 @@
+Math //all constant and values will appear
+
+// Methods of Maths
+
+// Math.abs(n) will give absoule value
+// Math.floor(n) will give whole and low value nearnest smallest integer
+// Math.pow(n) will give value of power
+// Math.ceil(n) will round off to the largest  
