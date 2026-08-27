068b879e65089d105eab206051b156f8791fd5b8
fatima gul <fatimaguldev@gmail.com>
2026-08-27 23:41:22 +0500

    updated object practice 4

---
+++ JS_prac_set/ObjectPrac4.js
@@ -0,0 +1,8 @@
+// Create an object representing a car that stores the following properties for the car: name,model,color.Print the car’s name.
+
+let car = {
+    name: "Lamborgini",
+    model: 2026,
+    color: "Yellow"
+};
+console.log(car.name);
