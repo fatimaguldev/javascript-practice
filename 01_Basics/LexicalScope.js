235cda0380ccd300028a05104f3b4865b0769600
fatima gul <fatimaguldev@gmail.com>
2026-08-29 23:52:32 +0500

    lexical scope

---
+++ 01_Basics/LexicalScope.js
@@ -0,0 +1,12 @@
+// Lexical scope
+
+function outerFunc() {
+    let x = 5;
+    let y = 6;
+    function innerFunc() {
+        console.log(x);
+        console.log(y);
+        
+    }
+    innerFunc();
+}
