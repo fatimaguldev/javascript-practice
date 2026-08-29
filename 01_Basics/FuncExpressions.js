3b9536bfc5bd05577124f178120f2efc9b1c9771
fatima gul <fatimaguldev@gmail.com>
2026-08-30 01:28:09 +0500

    updated function expressions

---
+++ 01_Basics/FuncExpressions.js
@@ -0,0 +1,11 @@
+// nameless function
+// method of definning a function 
+
+let name = "fatima gul";
+
+let sum = function (a, b) {
+    return a + b;
+
+}
+
+// here sum is a variable name and we call it as a function 
