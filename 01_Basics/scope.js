5302c7b6e403b100c39f94655f2ed967542afad6
fatima gul <fatimaguldev@gmail.com>
2026-08-29 23:52:53 +0500

    scope

---
+++ 01_Basics/scope.js
@@ -0,0 +1,11 @@
+function calSum(a, b) {
+    let sum = a + b; //function scope 
+    console.log(sum); //agar isko hum function k bahir lakhi tu result nahi ayga 
+    
+} 
+// console.log(sum); no result so its called FUNCTION SCOPE
+
+// and if we define valriable outside the funstion it is called GLOBAL SCOPE 
+sum = 56; //global scope
+
+
