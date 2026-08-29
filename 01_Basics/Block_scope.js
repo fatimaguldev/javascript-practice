2a4d05f064b59ac62cf8142971a3b15fc4bca647
fatima gul <fatimaguldev@gmail.com>
2026-08-29 23:52:21 +0500

    block scope

---
+++ 01_Basics/Block_scope.js
@@ -0,0 +1,11 @@
+// Block Scope
+
+{
+    let a = 1; //aslo cont keyword not work
+
+}
+console.log(a); //this will not print becuase it is only accessible within the brackets
+
+for (let i = 1; i < 5; i++){
+    console.log(i);  //block scope
+}
