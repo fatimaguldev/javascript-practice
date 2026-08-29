3df0866e1e7591b1de14089003104645c5630554
fatima gul <fatimaguldev@gmail.com>
2026-08-30 01:28:35 +0500

    methods functions

---
+++ 01_Basics/MethodsFunc.js
@@ -0,0 +1,12 @@
+const calculator = {
+    num:50,
+  add: function (a, b) {
+    return a + b;
+  },
+  sub: function (a, b) {
+    return a - b;
+  },
+  mul: function (a, b) {
+    return a * b;
+  },
+};
