726f9bd389c25d947fe5a295e5cea95a0c67455a
fatima gul <fatimaguldev@gmail.com>
2026-08-30 01:28:58 +0500

    function practice 7

--- JS_prac_set/FuncPrac7.js
+++ JS_prac_set/FuncPrac7.js
@@ -1,16 +1,16 @@
 // what will be the output
 
-let greet = "hello";
+let greet = "hello"; //global scope
 
 function changeGreet() {
-  let greet = "hi";
+  let greet = "hi"; //function scope
   console.log(greet);
-  function innerGreen() {
-    console.log(greet);
+  function innerGreet() {
+    console.log(greet); //lexical scope will not run ..bcz we did not call
   }
 }
 
-console.log(greet);
-changeGreet();
+console.log(greet); //this will return hello
+changeGreet(); //this will return hi
 
 // output will be "hello hi"
