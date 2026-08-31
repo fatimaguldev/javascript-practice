ee066cce34c95a90887f48b94aff4f252d5879ed
fatima gul <fatimaguldev@gmail.com>
2026-08-31 16:46:06 +0500

    JS try and catch

---
+++ 01_Basics/tryAndCatch.js
@@ -0,0 +1,13 @@
+console.log("hello");
+console.log("hello");
+console.log("hello");
+// let b = 6;
+try {
+    console.log(b);
+    
+} catch (err){
+    console.log("b is not defined, caught error");
+    console.log(err);
+    
+}
+console.log("hello 2");
