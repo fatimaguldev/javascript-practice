11e3e0ecf6c77617f7159b6e6b8d8c3cbc73fe41
fatima gul <fatimaguldev@gmail.com>
2026-09-01 23:37:08 +0500

    add filter function

---
+++ 01_Basics/FilterFunc.js
@@ -0,0 +1,4 @@
+let nums = [10, 12, 11, 15, 23, 11, 16];
+let ans = nums.filter((el) => { 
+    return el % 2 == 0; // for even it will be true and for odd it will be false for odd it will be el%2 !=0;
+});
