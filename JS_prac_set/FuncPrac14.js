3c6240f2f040d0ea298a993f42e2b1216d8e10a8
fatima gul <fatimaguldev@gmail.com>
2026-09-05 23:39:43 +0500

    Update FuncPrac14.js

---
+++ JS_prac_set/FuncPrac14.js
@@ -0,0 +1,9 @@
+// Create a new array using the map function whose each element is equal to the original element plus 5.
+
+const originalArray =[10,20,30,40,50];
+
+// Create a new array with each element + 5
+const newArray = originalArray.map(num => num + 5);
+
+console.log(newArray); 
+// Output: [15, 25, 35, 45, 55]
