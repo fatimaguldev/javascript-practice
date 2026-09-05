fc3312a655c46f6a911bb8b7065cc44bf0c4af45
fatima gul <fatimaguldev@gmail.com>
2026-09-05 23:39:41 +0500

    Update spread.js

---
+++ 01_Basics/spread.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3, 4, 5, 6, 8, 9, 0, 6, 5, 4, 32, 2, 4, 5, 67, 8, 92, 1, 2, 3, 4668];
+// to use the spread concept we have to access it through math
+
+Math.min(...arr);
+Math.max(...arr);
