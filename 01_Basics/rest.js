8f5ac56eb0400ebea98f3d406dc75da2046ccd63
fatima gul <fatimaguldev@gmail.com>
2026-09-05 23:39:40 +0500

    Update rest.js

---
+++ 01_Basics/rest.js
@@ -0,0 +1,9 @@
+// opp of speard concept ...from multiple to one
+
+function sum(...args) {
+    for (let i = 0; i < args.length; i++)
+    {
+        console.log("you gave us:", args[i])
+            ;
+    }
+}
