a8b67a1c1f712028b8f045192d450be757521fa3
fatima gul <fatimaguldev@gmail.com>
2026-09-07 23:24:38 +0500

    ClassListProp.js

---
+++ Advance_JS/ClassListProp.js
@@ -0,0 +1,8 @@
+let heading = document.querySelector('h1');
+heading.classList.add("green");
+
+heading.classList.contains("underline"); //false
+
+heading.classList.remove("green"); //green color will be remove
+
+heading.classList.toggle(); //between add and remove
