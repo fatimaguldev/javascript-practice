3fc9b8168735df3f8cc2b1bb2f72c4fd4bdd9d7f
fatima gul <fatimaguldev@gmail.com>
2026-09-07 23:24:43 +0500

    ManipulatingAtt.js

---
+++ Advance_JS/ManipulatingAtt.js
@@ -0,0 +1,9 @@
+// obj.getAttribute(attr);
+
+// obj.setAttribute(arr, val);
+
+let links = document.querySelectorAll(".box a");
+
+for (let i = 0; i < links.length; i++) {
+  links[i].style.color = "yellow";
+}
