ad90f594922f3812799f48b7707ebe7f501c2b2f
fatima gul <fatimaguldev@gmail.com>
2026-09-07 23:24:43 +0500

    QuerySelectors.js

---
+++ Advance_JS/QuerySelectors.js
@@ -0,0 +1,13 @@
+console.dir(document.querySelector("h1"));
+
+console.dir(document.querySelector("p"));
+
+console.dir(document.querySelector("#description"));
+
+console.dir(document.querySelector("p")); //first paragraph
+
+console.dir(document.querySelector(".oldImg"));  //only first img will be select
+
+console.dir(document.querySelector("div a")); //first link will be displayed
+
+console.dir(document.querySelectorAll("div a")); //it will select all the elements
