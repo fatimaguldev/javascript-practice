96c1b907b452de3034fccc8b631dcc96b8a22df5
fatima gul <fatimaguldev@gmail.com>
2026-09-09 23:56:37 +0500

    DOM.js

--- Advance_JS/DOM.js
+++ Advance_JS/DOM.js
@@ -11,14 +11,13 @@ console.dir(document.querySelector("p"));
 
 console.dir(document.querySelector("#description"));
 
-console.dir(document.querySelector('p'));  //first paragraph
+console.dir(document.querySelector("p")); //first paragraph
 
 console.dir(document.querySelector(".oldImg"));
 
 console.dir(document.querySelector("div a")); //first link will be displayed
 
 console.dir(document.querySelectorAll("div a")); //it will select all the links
-
 
 let links = document.querySelectorAll(".box a");
 
@@ -27,4 +26,46 @@ for (let i = 0; i < links.length; i++) {
 }
 
 let heading = document.querySelector("h1");
-heading.classList.add("green");  //we have to change or add this to css file also 
+heading.classList.add("green"); //we have to change or add this to css file also
+
+let h4 = document.querySelector("h4");
+console.log(h4);
+console.log(h4.parentElement);
+
+// To select an element by its class name, you must include a dot (.) before the class name inside querySelector.
+
+// Add the dot (.) before box to target the class
+
+let box = document.querySelector(".box");
+console.log(box.children);
+
+let newP = document.createElement("p");
+console.dir(newP);
+
+newP.innerText = "hi, i am a new para";
+
+let body = document.querySelector("body");
+
+body.appendChild(newP); //append means add at the end
+
+
+// 1. Create the button element
+let btn = document.createElement("button");
+btn.innerText = "Click me";
+
+// 2. Style the button (Optional: styling matches Spider-Man!)
+btn.style.backgroundColor = "#E62429"; // Marvel Red
+btn.style.color = "white";
+btn.style.padding = "10px 15px";
+btn.style.border = "none";
+btn.style.borderRadius = "5px";
+btn.style.cursor = "pointer";
+btn.style.marginTop = "15px";
+
+// 3. Append it using your existing 'box' variable
+box.appendChild(btn);
+
+// 4. Make it do something when clicked
+btn.addEventListener("click", function() {
+  alert("Friendly neighborhood Spider-Man says hi! ");
+});
