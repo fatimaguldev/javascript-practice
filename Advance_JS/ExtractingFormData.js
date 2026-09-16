b8534e2fadfa12269a5f4fbaa7b5fa8ecd440c37
fatima gul <fatimaguldev@gmail.com>
2026-09-16 23:59:18 +0500

    ExtractingFormData.js

---
+++ Advance_JS/ExtractingFormData.js
@@ -0,0 +1,13 @@
+let form = document.querySelector("form");
+
+document.addEventListener("submit", function (event) {
+  event.preventDefault(); //action ko rokhny k leye
+  
+    
+    let inp = document.querySelector("input")
+    
+    console.dir(inp);
+    console.log(inp.value); //by user
+    
+    
+});
