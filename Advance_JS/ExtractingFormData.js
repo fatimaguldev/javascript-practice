8473862cd3ec35ba263bcfbfea1c2e1eb9b54a6c
fatima gul <fatimaguldev@gmail.com>
2026-09-17 22:24:00 +0500

    ExtractingFormData.js

--- Advance_JS/ExtractingFormData.js
+++ Advance_JS/ExtractingFormData.js
@@ -4,10 +4,11 @@ document.addEventListener("submit", function (event) {
   event.preventDefault(); //action ko rokhny k leye
   
     
-    let inp = document.querySelector("input")
+    let user = document.querySelector("#user")
+    let pass = document.querySelector("#pass");
     
     console.dir(inp);
     console.log(inp.value); //by user
     
-    
+    alert(`Hi, ${user.value}, your password is set to ${pass.value}`)
 });
