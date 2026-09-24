04e5edcc27d8a1782f7dfb982f11d908712c189d
fatima gul <your-github-verified-email@example.com>
2026-09-24 23:54:49 +0500

    SingleThread.js

---
+++ Advance_JS/SingleThread.js
@@ -0,0 +1,16 @@
+let a = 25;
+console.log(a);
+let b = 40;
+console.log(b);
+console.log(a + b);
+
+
+setTimeout(function () {
+    console.log("fatima gul");
+    
+}, 2000);
+
+console.log("hello");
+
+
+
